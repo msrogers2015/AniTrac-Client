fca00fff000fd11f81225b19825a57e59b23ae09
marquel-rogers <144250049+marquel-rogers@users.noreply.github.com>
2024-10-19 10:30:33 -0500

    Dashboard data population and Menu creation.
    For the menu, users can now preform a server check, see what server is being connected to and change the server to connect to. There is also a premilinary
    help menu which is display a local html file for documenation.
    
    For the dashboard, data is requested from the server and populated into the table. If there is an issue connecting to the server, the user is alerted via an
    error dialog box.
    
    Some additional information has been added to the config.json file for later use and a theme.json file has been created to track theme changes for the local
    instance of the software.

--- anitrac/anitrac/dashboard.py
+++ anitrac/anitrac/dashboard.py
@@ -1,29 +1,75 @@
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import tkinter as tk
+import requests
 
 class Dashboard(tk.Frame):
-    def __init__(self):
+    def __init__(self, width, height, server):
         super().__init__()
+        self.w = width
+        self.h = height
+        self.server = server
         self.create_table()
         self.create_widets()
+        self.populate_table()
 
     def create_table(self):
-        self.table = ttk.Treeview(self)
+        self.table = ttk.Treeview(self, columns=('Anilox','Milage', 'Clean Cycles'), show='headings')
         self.yscroll = ttk.Scrollbar(self, orient="vertical")
         self.yscroll.config(command=self.table.yview)
         self.table.config(yscrollcommand=self.yscroll.set, selectmode="extended")
-        self.table['columns'] = ['Anilox','Milage', 'Clean Cycles']
          # Phantom Header
         self.table.column("#0", width=0, minwidth=0)
         self.table.heading("#0", text="PID")
         for index, col in enumerate(self.table["columns"]):
             self.table.column(col, anchor="center")
             self.table.heading(col, text=col, anchor="center")
-        self.table["show"] = "headings"
         self.table.place(relx=(5/600), rely=(10/400), relheight=(325/400), relwidth=(580/600))
         self.yscroll.place(relx=(585/600), rely=(10/400), relheight=(325/400), relwidth=(15/600))
 
+        self.table.tag_configure("even", background="#a7a8a9")
+        self.table.tag_configure("odd", background="#ffffff")
+        self.table.tag_configure('close', background='#fce300')
+        self.table.tag_configure('over', background='#c8102e')
+
+    def server_check(self):
+        try:
+            r = requests.get(self.server)
+            if r.status_code == 200:
+                return True
+        except requests.exceptions.ConnectionError:
+            messagebox.showerror(title='Connection Error', message=f'Connection timeout, could not connect to {self.server}')
+            return False
+        except Exception as e:
+            messagebox.showerror(title='Uncaught Error',message=f'{e.__class__}\n{e}')
+            return False
+
     def create_widets(self):
         clean = ttk.Button(self, text='Clean Anilox')
-        clean.place(relx=(200/600), rely=(350/400), relheight=(40/400), relwidth=(200/600))
+        clean.place(relx=(50/self.w), rely=(350/self.h), relheight=(40/self.h), relwidth=(200/self.w))
+        refresh = ttk.Button(self, text='Refresh Table', command=self.update_table)
+        refresh.place(relx=(350/self.w), rely=(350/self.h), relheight=(40/self.h), relwidth=(200/self.w))
 
+    def populate_table(self):
+        if self.server_check():
+            endpoint = self.server + '/anilox_list'
+            data = requests.get(endpoint).json()
+            for index, record in enumerate(data):
+                if index % 2 == 0:
+                    tag = ('even',)
+                if index % 2 == 1:
+                    tag = ('odd',)
+                roller = record['roller']
+                milage = record['milage']
+                cycles = record['clean_cycles']
+                self.table.insert(
+                    parent='', index=index, values=[roller, f'{milage:,}', f'{cycles:,}'],tags=tag)
+    
+    def clear_table(self) -> None:
+        """Delete all records from table"""
+        for record in self.table.get_children():
+            self.table.delete(record)
+
+    def update_table(self) -> None:
+        """Clean and repopulate table with search filters"""
+        self.clear_table()
+        self.populate_table()
--- anitrac/anitrac/main.py
+++ anitrac/anitrac/main.py
@@ -1,41 +1,52 @@
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import tkinter as tk
 import dashboard
+import json
+import requests
 
 class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        w = 600
-        h = 400
-        x = (self.winfo_screenwidth() // 2) - (w // 2)
-        y = (self.winfo_screenheight() // 2) - (h // 2)
-        self.minsize(w,h)
-        self.geometry(f'{w}x{h}+{x}+{y}')
+        self.w = 600
+        self.h = 400
+        x = (self.winfo_screenwidth() // 2) - (self.w // 2)
+        y = (self.winfo_screenheight() // 2) - (self.h // 2)
+        self.minsize(self.w, self.h)
+        self.geometry(f'{self.w}x{self.h}+{x}+{y}')
         self.title('AniTrac - Dashboard')
         self.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.server = self.get_server()
         self.set_style()
+        self.select_server()
         self.create_notebook()
         self.create_menu()
         self.bind('<Configure>', self.on_resize)
         self.mainloop()
-    
+
+    def get_server(self):
+        with open('config.json', 'r') as file:
+            json_data = json.load(file)
+            return json_data['server']
+
     def on_resize(self, event):
         h = self.winfo_height()
         fontsize = h//40
         self.style.configure('.', font=(None, fontsize))
         self.style.configure('Treeview.Heading', font=(None, fontsize))
+        self.style.configure('Treeview', rowheight=h//20)
 
     def set_style(self) -> None:
         self.style = ttk.Style()
         #style.theme_use('alt')
         self.style.configure('TNotebook.Tab', width=self.winfo_screenwidth())
-        self.style.configure('TNotebook.Tab', sticky='we')
+        self.style.configure('server.TButton', font=(None, 14))
+        self.style.configure('Treeview', rowheight=50)
 
     def create_notebook(self) -> None:
         self.notebook = ttk.Notebook(self)
         self.notebook.pack(fill='both', side='top', expand=True)
-        self.dashboard = dashboard.Dashboard()
+        self.dashboard = dashboard.Dashboard(self.w, self.h, self.server)
         frame2 = ttk.Frame(self.notebook)
         self.dashboard.pack()
         frame2.pack()
@@ -46,15 +57,61 @@ class App(tk.Tk):
         menubar = tk.Menu(self)
         file = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label='File', menu=file)
-        file.add_command(label='Check Server')
-        file.add_cascade(label='Select Server')
+        file.add_command(label='Current Server', command=self.current_server)
+        file.add_command(label='Check Server', command=self.check_server)
+        file.add_cascade(label='Select Server', command=self.select_top.deiconify)
         help = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label='Help', menu=help)
         help.add_command(label="Help")
         help.add_command(label='About Anitrac')
-
         self.config(menu=menubar)
 
+    def current_server(self):
+        messagebox.showinfo(title='Current Server', message=self.server)
+
+    def select_server(self):
+        self.select_top = tk.Toplevel(self)
+        self.select_top.protocol("WM_DELETE_WINDOW", self.select_top.withdraw)
+        w=400
+        h=125
+        x = (self.winfo_screenwidth() // 2) - (w // 2)
+        y = (self.winfo_screenheight() // 2) - (h // 2)
+        self.select_top.minsize(w,h)
+        self.select_top.geometry(f'{w}x{h}+{x}+{y}')
+        self.current = ttk.Label(self.select_top, text='Current Server: ' + self.server, font=(None, 14), anchor='center')
+        self.current.pack(fill='x')
+        self.new_server = ttk.Entry(self.select_top, font=(None, 14))
+        self.new_server.pack(fill='x', padx=25, pady=5)
+        save = ttk.Button(self.select_top, text='Save Server', style='server.TButton', command=lambda: self.save_server(self.new_server.get()))
+        save.pack(fill='x', padx=100, pady=5, ipady=7)
+        self.select_top.withdraw()
+
+    def save_server(self, server):
+        if server != '':
+            if messagebox.askokcancel(title='Save Server', message=f'Save {server} as new server?'):
+                self.server = server
+                with open('config.json', 'r') as file:
+                    json_data = json.load(file)
+                json_data['server'] = self.server
+                with open('config.json', 'w') as file:
+                    file.write(json.dumps(json_data, indent=4))
+                self.new_server.delete(0, 'end')
+                self.current.config(text=f'Current Server: {self.server}')
+                self.select_top.withdraw()
+                self.dashboard.server = server
+                return True
+        self.select_top.focus_force()
+        return False
+
+    def check_server(self):
+        try:
+            r = requests.get(self.server)
+            if r.status_code == 200:
+                messagebox.showinfo(title='Valid Connection', message=f'You are connected to {self.server}')
+        except requests.exceptions.ConnectionError:
+            messagebox.showerror(title='Connection Error', message=f'Connection timeout, could not connect to {self.server}')
+        except Exception as e:
+            messagebox.showerror(title='Uncaught Error',message=f'{e.__class__}\n{e}')
 
     def on_close(self) -> None:
         try:
